d00b6457fb1f64396de3436616d87f8d8be77613
Mehmet Akif Ersoy <makifersoy2006@gmail.com>
2023-11-11 17:28:11 +0300

    fixes the big int issue, fixed a bug in scatter isValid fucntion

--- contracts/src/boxes/Scatter.ts
+++ contracts/src/boxes/Scatter.ts
@@ -8,9 +8,8 @@ export function isValidScatter(fields: BoxFields): Bool {
     DIR.ALL.map((d) => {
       // is this item looking at this direction?
       const isItemDir = fields.itemDir.equals(d);
-      const d_l = (d - 3) % 8;
+      const d_l = (d - 3 + 8) % 8;
       const d_r = (d + 3) % 8;
-
       // is the item valid for this direction?
       // there are multiple steps to this, shown below.
 
@@ -22,17 +21,15 @@ export function isValidScatter(fields: BoxFields): Bool {
 
       // - if the `in` at (d-3) or (d+3) is true than `out` d should be true
       const isValidCorners = fields.outs[d].equals(
-        fields.outs[d_l].or(fields.outs[d_r])
+        fields.ins[d_l].or(fields.ins[d_r])
       );
 
       // - the other output signals should be 0 (except d, d-3, d+3)
-      const isValidOut = fields.outs[d - 1]
-        .equals(false)
-        .and(fields.outs[(d + 1) % 8].equals(false))
-        .and(fields.outs[(d + 2) % 8].equals(false))
-        .and(fields.outs[(d + 4) % 8].equals(false))
-        .and(fields.outs[(d + 6) % 8].equals(false))
-        .and(fields.outs[(d + 7) % 8].equals(false));
+      const isValidOut = fields.outs[(d + 1) % 8]
+        .or(fields.outs[(d + 2) % 8])
+        .or(fields.outs[(d + 4) % 8])
+        .or(fields.outs[(d + 6) % 8])
+        .or(fields.outs[(d + 7) % 8]).not();
 
       // finally `and` them all
       const isValid = isValidDirect.and(isValidCorners).and(isValidOut);
