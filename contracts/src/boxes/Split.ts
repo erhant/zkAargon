7f2fad2abc7ed6a1c0c7b67fef14147b36f49cc9
Mehmet Akif Ersoy <makifersoy2006@gmail.com>
2023-11-11 11:55:12 +0300

    updated split box implementation

--- contracts/src/boxes/Split.ts
+++ contracts/src/boxes/Split.ts
@@ -5,9 +5,8 @@ import { DIR } from '../utils/Direction';
 /** A Split box splits the input laser.
  *
  * If the laser is coming diagonally w.r.t the mirror direction,
- * it should split the laser to the (d+2) and (d-2) directions; or,
- * a laser is coming from horizontally w.r.t. the mirror direction it should go directly without an intervention,
- * ; in other cases the laser should not propagate.
+ * it should split the laser to the (d+2) and (d-2) directions;
+ * or, in other cases the laser should not propagate.
  *
  * There shouldn't be any other out signals.
  */
@@ -28,22 +27,14 @@ export class SplitBox extends Box {
         // is the item valid for this direction?
         // there are multiple steps to this, shown below.
 
-        // - the `in` and `out` at `d` should split the laser
-        // if no direct laser then we should not check d_l and d_r
-        const notDirect = this.ins[d].not();
-        const isValidDirect = notDirect.or(
-          this.ins[d].and(this.outs[d_r]).and(this.outs[d_l])
-        );
-
-        // - the `in` at d-2 should equal `out` d+2
-        const isValidRightThrough = this.ins[d_l].equals(this.outs[d_r]);
-
-        // - the `in` at d+2 should equal `out` d-2
-        const isValidLeftThrough = this.ins[d_r].equals(this.outs[d_l]);
+        // - the `in` at `d` should split the laser
+        // so the `in` at d should equal `out` d+2 and `out`d-2
+        const isValidDirect = this.ins[d].equals(this.outs[d_r]).and(this.ins[d].equals(this.outs[d_l]))
 
         // - the other output signals should be 0
-        const isValidOut = this.outs[(d + 1) % 8]
+        const isValidOut = this.outs[d]
           .equals(false)
+          .and(this.outs[(d + 1) % 8].equals(false))
           .and(this.outs[(d + 3) % 8].equals(false))
           .and(this.outs[(d + 4) % 8].equals(false))
           .and(this.outs[(d + 5) % 8].equals(false))
@@ -51,8 +42,6 @@ export class SplitBox extends Box {
 
         // finally `and` them all
         const isValid = isValidDirect
-          .and(isValidRightThrough)
-          .and(isValidLeftThrough)
           .and(isValidOut);
 
         return isItemDir.and(isValid);
